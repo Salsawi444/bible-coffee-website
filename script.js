37ffab416c44da7af17798e74a9eede567296a98
Salsawi444 <familysalsawi@gmail.com>
2026-02-11 20:36:12 +0300

    Update script.js

--- script.js
+++ script.js
@@ -57,3 +57,13 @@ form.addEventListener('submit', async function(e) {
 
   form.reset();
 });
+
+// Toggle Hamburger Menu
+document.getElementById('menuToggle').addEventListener('click', function() {
+  const menu = document.getElementById('hamburgerMenu');
+  if (menu.style.display === 'block') {
+    menu.style.display = 'none';
+  } else {
+    menu.style.display = 'block';
+  }
+});

00045ddc1c512814e61807daf683b0b2b78865cb
Salsawi444 <familysalsawi@gmail.com>
2026-02-11 12:21:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 
+// Initial slots per location
+const slots = {
+  Bali: 8,
+  Mexico: 8
+};
+
+const form = document.getElementById('registrationForm');
+const availability = document.getElementById('availability');
+
+// Show initial availability
+function updateAvailability() {
+  availability.innerHTML = `
+    Bali: ${slots.Bali} spots left<br>
+    Mexico: ${slots.Mexico} spots left
+  `;
+}
+updateAvailability();
+
+// Handle registration
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  const location = document.getElementById('location').value;
+
+  if (slots[location] > 0) {
+    slots[location]--;
+    alert(`Registered for ${location}!`);
+  } else {
+    alert(`Sorry, ${location} is full.`);
+  }
+  updateAvailability();
+});
